11872b8a16b960e458865f4aa74c1d332af53a8a
akirafn <flavio.nakahara@gmail.com>
2017-07-13 18:03:26 -0300

    First commit

---
+++ iFoodPlannerSwift/ViewController/VCCozinhaLista.swift
@@ -0,0 +1,46 @@
+//
+//  VCCozinhaLista.swift
+//  iFoodPlannerSwift
+//
+//  Created by Flavio Akira Nakahara on 7/13/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+import UIKit
+
+class VCCozinhaLista: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
